151386f0b87f1db14278883e546f9d976f171307
LenardLn <nagylenard2002@gmail.com>
2024-11-06 22:36:24 +0200

    updating again the update

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -4,14 +4,14 @@ import { NextResponse } from "next/server";
 
 export async function PUT(req: Request) {
   const session = await auth();
-  const currentUserEmail = session?.user?.email!;
+  const currentUser = session?.user?.name!;
 
   const data = await req.json();
   data.age = Number(data.age);
 
   const user = await prisma.user.update({
     where: {
-      email: currentUserEmail,
+      name: currentUser,
     },
     data,
   });
